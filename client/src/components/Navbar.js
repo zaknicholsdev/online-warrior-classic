1472be7c1eef2726488d8bbfc5ab6e3a5d95314b
zaknicholsdev <zaknicholsdev1993@gmail.com>
2020-10-10 01:19:37 -0400

    css, changed start script, fixed router links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,17 +13,17 @@ const Navbar = () => {
             <nav>
                 <div className="navWide">
                     <div className="wideDiv">
-                        <Link to="">Home</Link>
-                        <Link to="">Athletes</Link>
-                        <Link to="">Login</Link>
+                        <Link to="/">Home</Link>
+                        <Link to="athletes">Athletes</Link>
+                        <Link to="/login">Login</Link>
                     </div>
                 </div>
                 <div className="navNarrow" onClick={handleToggle}>
                     <i className="fa fa-bars fa-2x"></i>
                     <div ref={narrowLinks} className="narrowLinks hidden">
-                        <Link to="">Home</Link>
-                        <Link to="">Athletes</Link>
-                        <Link to="">Login</Link>
+                        <Link to="/">Home</Link>
+                        <Link to="/athletes">Athletes</Link>
+                        <Link to="/login">Login</Link>
                     </div>
                 </div>
             </nav>
